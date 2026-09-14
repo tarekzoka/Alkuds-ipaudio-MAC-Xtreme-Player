7ba787a419831caace97873320e86080c8af4f20
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2026-09-14 21:28:51 +0300

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "=========================================================="
+echo "      Downloading Alkuds ipaudio-MAC-Xtreme-Player        "
+echo "=========================================================="
+
+rm -f /tmp/Alkuds_ipaudio.ipk
+
+wget -O /tmp/Alkuds_ipaudio.ipk "https://raw.githubusercontent.com/xximhxx/Alkuds-ipaudio-MAC-Xtreme-Player/main/Alkuds_ipaudio-R43.ipk"
+
+echo "=========================================================="
+echo "               Installing Plugin...                       "
+echo "=========================================================="
+
+# تمت إضافة --force-reinstall هنا
+opkg install --force-reinstall --force-overwrite /tmp/Alkuds_ipaudio.ipk
+
+rm -f /tmp/Alkuds_ipaudio.ipk
+
+echo "=========================================================="
+echo "           Installation Completed Successfully!           "
+echo "               Restarting Enigma2 GUI...                  "
+echo "=========================================================="
+
+killall -9 enigma2
+exit 0
